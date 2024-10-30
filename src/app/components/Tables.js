33fadb0fe78645cf98f80cf9a6d26d37014b8e42
Juliet <juliet.kegwuonu.com>
2024-10-30 15:56:27 +0100

    first

--- src/app/components/Tables.js
+++ src/app/components/Tables.js
@@ -116,7 +116,7 @@ const deleteCapsule=(params)=>{
 
     <div className="mt-6 md:flex md:items-center md:justify-between">
     <div className='flex items-center gap-6'>
-    <Filter name='status' one="active" two='unknowm' three='retired' setTableData={setTableData}/>
+    <Filter name='status' one="active" two='unknown' three='retired' setTableData={setTableData}/>
     <Filter name='type' one="Dragon 1.0" two='Dragon 1.1' three='Dragon 2.0' setTableData={setTableData}/>
     </div>
         
@@ -129,7 +129,7 @@ const deleteCapsule=(params)=>{
                 </svg>
             </span>
 
-            <input onChange={(e)=>search(e.target.value)} type="text" placeholder="Search" className="block w-full py-1.5 pr-5 text-gray-700 bg-white border border-gray-200 rounded-lg md:w-80 placeholder-gray-400/70 pl-11 rtl:pr-11 rtl:pl-5 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"/>
+            <input onChange={(e)=>search(e.target.value)} type="text" placeholder="Search capsule id" className="block w-full py-1.5 pr-5 text-gray-700 bg-white border border-gray-200 rounded-lg md:w-80 placeholder-gray-400/70 pl-11 rtl:pr-11 rtl:pl-5 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"/>
         </div>
     </div>
 
